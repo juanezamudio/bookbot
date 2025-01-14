add81bd3119f32cea380dd505eccaee5d1761f76
Juan Zamudio <jzamudio14@gmail.com>
2025-01-14 14:54:25 -0500

    refactored bookbot project

--- main.py
+++ main.py
@@ -14,9 +14,8 @@ def book_text(file_path):
     with open(file_path) as f:
         return f.read()
 
-def count_words(text):
-    words = text.split()
-    return len(words)
+def count_words(text): 
+    return len(text.split())
 
 def count_letters(text):
     letters = {}
